b42ed344dafb93fdf412cd49c11cbe0126778d82
HadiAlizade77 <alizadehadi08@gmail.com>
2019-10-20 13:54:12 +0200

    Backend bugfix

---
+++ server/models/client.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require('mongoose-type-email');
+
+const clientSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    email: { type: mongoose.SchemaTypes.Email, allowBlank: true },
+    phone: String,
+    providers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Provider' }]
+});
+
+module.exports = mongoose.model('Client', clientSchema);
